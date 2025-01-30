4d9e2f15337c101866c191c8f6a29e62e0841a55
Roberto Linares <blendboy@gmail.com>
2025-01-29 18:04:27 -0600

    Adicion de JWT

--- src/main/java/com/myfinance/controller/SessionController.java
+++ src/main/java/com/myfinance/controller/SessionController.java
@@ -3,7 +3,10 @@ package com.myfinance.controller;
 import com.myfinance.dto.LoginRequest;
 import com.myfinance.model.Usuario;
 import com.myfinance.service.UsuarioService;
+import com.myfinance.util.JwtUtil;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,14 +20,20 @@ public class SessionController {
     @Autowired
     private UsuarioService usuarioService;
 
+     @Autowired
+    private JwtUtil jwtUtil;
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         Optional<Usuario> usuario = usuarioService.findByUsuario(loginRequest.getUsuario());
-        
+
         if (usuario.isPresent() && usuarioService.validateUserPassword(usuario.get().getId(), loginRequest.getPassword())) {
-            return ResponseEntity.ok().build();
+            String token = jwtUtil.generateToken(loginRequest.getUsuario());
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + token);
+            return ResponseEntity.ok().headers(headers).build();
         }
-        
+
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 }
